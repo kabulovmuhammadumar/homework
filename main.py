99a00796545012a210458a03f1a070cc4570a03c
faruqiy <qobiljonovmuhammadumar3@gmail.com>
2025-02-24 21:14:47 +0500

    apt apt

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("faruqiy")
+# githubda repository ochamiz
+# add readme file ni tanlab qo'yamiz
+# ssh urlni copy qilamiz
+# pychar yoki vscode terminaliga yozamiz
+# git clone url
+# nimadir o'zgartiramiz
+# git add .
+# git commit -m "nimadir"
+# git push
